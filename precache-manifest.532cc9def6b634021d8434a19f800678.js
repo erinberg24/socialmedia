52f5ff807905a384dc821f8fa4860aa0768b33f9
erinberg24 <bergec@bc.edu>
2020-02-24 11:00:08 -0500

    Updates

---
+++ precache-manifest.532cc9def6b634021d8434a19f800678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244a934ac17a0fd47b538d04043ca69e",
+    "url": "/social-media-erinberg24/index.html"
+  },
+  {
+    "revision": "093c54043b54f0a51c5c",
+    "url": "/social-media-erinberg24/static/css/main.619d2d2f.chunk.css"
+  },
+  {
+    "revision": "0c8445885e5e5c9090a0",
+    "url": "/social-media-erinberg24/static/js/2.d9eeb445.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/social-media-erinberg24/static/js/2.d9eeb445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093c54043b54f0a51c5c",
+    "url": "/social-media-erinberg24/static/js/main.cb8f3c4f.chunk.js"
+  },
+  {
+    "revision": "5c30b0101fca435d1256",
+    "url": "/social-media-erinberg24/static/js/runtime-main.72d1b2dc.js"
+  }
+]);
