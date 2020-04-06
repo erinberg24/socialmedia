7899507dad114290a07a2a8503b2b47e368cdb1f
Erin Berg <erinberg@Erins-MacBook-Pro-2.local>
2020-04-05 22:14:24 -0700

    Implemented Firestore

--- src/contexts/StoreContext.js
+++ src/contexts/StoreContext.js
@@ -1,21 +1,38 @@
 import React, {createContext, useState, useEffect} from 'react';
 import uniqueId from 'utils/uniqueId.js';
 import initialStore from 'utils/initialStore.js';
+import * as firebase from 'firebase';
+import 'firebase/database'
 
 // export the context so that other components can import
 export const StoreContext = createContext(); 
 
 function StoreContextProvider(props){
 
-    //const [store, setStore] = useState(initialStore);
+    // //const [store, setStore] = useState(initialStore);
 
-    const [store, setStore] = useState(()=>{
-        return JSON.parse(window.localStorage.getItem('store')) || initialStore;
-    });
+    // const [store, setStore] = useState(()=>{
+    //     return JSON.parse(window.localStorage.getItem('store')) || initialStore;
+    // });
 
-    useEffect(()=>{
-        window.localStorage.setItem('store', JSON.stringify(store));
-    }, [store]);
+    // useEffect(()=>{
+    //     window.localStorage.setItem('store', JSON.stringify(store));
+    // }, [store]);
+
+    const firebaseConfig = {
+
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+
+    const [currentUserId, setCurrentUserId] = useState(null); // or 'erin'
+    const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [likes, setLikes] = useState([]);
+    const [followers, setFollowers] = useState([]);
+    const [comments, setComments] = useState([]);
 
     function addLike(postId){
         const like = {
@@ -23,25 +40,27 @@ function StoreContextProvider(props){
             postId,
             datetime: new Date().toISOString()
         };
+
+        setLikes(likes.concat(like));
+        //Firestore update
+        db.collection('likes').add(like);
         
-        setStore({
-          ...store,
-          likes: store.likes.concat(like)
-        });
+        //setStore({
+          // ...store,
+          // likes: store.likes.concat(like)
+       // });
       }
     
-      function removeLike(postId){
-        // use filter and currentUserId to remove the like from the likes array
-        //const like = {
-        //  userId: store.currentUserId, 
-        //  postId,
-        //  datetime: new Date().toISOString()
-        //};
-        
-        setStore({
-          ...store,
-          likes: store.likes.filter(like=>!(like.userId===store.currentUserId && like.postId===postId))
-        });
+      function removeLike(postId){  
+        // setStore({
+        //   ...store,
+        //   likes: store.likes.filter(like=>!(like.userId===store.currentUserId && like.postId===postId))
+        // });
+
+        setLikes(likes.filter(like => !(like.userId === currentUserId && like.postId === postId)));
+
+        db.collection('likes').where('userId', '==', currentUserId).where('postId', '==', postId).get().then(snapshot=>snapshot.forEach(doc=>doc.ref.delete()));
+
       }
     
       function addComment(postId, text){
@@ -51,10 +70,15 @@ function StoreContextProvider(props){
           text,
           datetime: new Date().toISOString()
         };
-        setStore({
-          ...store,
-            comments:store.comments.concat(comment)
-        });
+
+        setComments(comments.concat(comment));
+        //Firestore update
+        db.collection('comments').add(comment);
+
+        // setStore({
+        //   ...store,
+        //     comments:store.comments.concat(comment)
+        // });
       }
     
       function addPost(photo, desc){
@@ -69,26 +93,64 @@ function StoreContextProvider(props){
           desc,
           datetime: new Date().toISOString()
         };
-        setStore({
-          ...store,
-          posts: store.posts.concat(post)
+
+        setPosts(posts.concat(post));
+        //Firestore update
+        db.collection('posts').add(post);
+
+        // setStore({
+        //   ...store,
+        //   posts: store.posts.concat(post)
     
-        })
+        // })
       }
       
       function addFollower(userId, followerId){
-        setStore({
-          ...store,
-          followers: store.followers.concat({userId, followerId})
-        });
+        // setStore({
+        //   ...store,
+        //   followers: store.followers.concat({userId, followerId})
+        // });
+        setFollowers(followers.concat(follower));
+        //Firestore update
+        db.collection('followers').add(follower);
+
       }
     
       function removeFollower(userId, followerId){
-          setStore({
-          ...store,
-          followers: store.followers.filter(follower=>follower.userId!==userId && follower.followerId!==followerId)
-        });
+        //   setStore({
+        //   ...store,
+        //   followers: store.followers.filter(follower=>follower.userId!==userId && follower.followerId!==followerId)
+        // });
+
+        setFollowers(followers.filter(follower => !(follower.userId === userId && folower.followerId === followerId)));
+
+        db.collection('followers').where('userId', '==', userId).where('followerId', '==', followerId).get().then(snapshot=>snapshot.forEach(doc=>doc.ref.delete()));
+        
+
       }
+
+  useEffect(()=>{
+    db.collection('users').get().then(snapshot=>{
+      const users = snapshot.docs.map(d=>d.data());
+      setUsers(users);
+    });
+    db.collection('posts').get().then(snapshot=>{
+      const posts = snapshot.docs.map(d=>d.data());
+      setPosts(posts);
+    });
+    db.collection('likes').get().then(snapshot=>{
+      const posts = snapshot.docs.map(d=>d.data());
+      setPosts(posts);
+    });
+    db.collection('followers').get().then(snapshot=>{
+      const posts = snapshot.docs.map(d=>d.data());
+      setPosts(posts);
+    });
+    db.collection('comments').get().then(snapshot=>{
+      const posts = snapshot.docs.map(d=>d.data());
+      setPosts(posts);
+    });
+  }, []);
 
 	return (
         <StoreContext.Provider value = {{...store, addComment, addLike, removeLike, addPost, addFollower, removeFollower}}>
